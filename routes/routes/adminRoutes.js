ec5adc85651a77de1fc69fb379c6d775e699b01e
Gideon Adzimah <gidadzimah@gmail.com>
2026-08-14 19:28:50 +0000

    Delete routes/routes/adminRoutes.js
    delete

--- routes/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Temp route so server doesn't crash
-router.get("/test", (req, res) => {
-  res.json({ message: "Admin routes are working" });
-});
-
-module.exports = router;
